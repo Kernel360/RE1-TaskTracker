4d238e1c6f2b7949729479d3b4494b4fce3b95c5
Chan <ieejo716@naver.com>
2024-02-25 12:36:26 +0900

    project setting

---
+++ src/main/java/org/TaskTracker/TaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.TaskTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/TaskTracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.TaskTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
